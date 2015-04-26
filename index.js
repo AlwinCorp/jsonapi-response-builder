6401e60dbce42613d8fb491a933398f24aae265c
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2015-04-26 16:49:08 +0200

    HOTFIX Export Error

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var MetaError   = require('./MetaError');
 
 module.exports.Response = MetaReponse;
 module.exports.Data     = MetaData;
-module.exports.Error    = MetaReponse;
+module.exports.Error    = MetaError;
